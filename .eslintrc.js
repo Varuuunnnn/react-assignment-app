78990928aaf5d88990f3839f8b787f7c8fb51c4c
varun <varun.gurjar@cilalabs.com>
2021-09-06 16:43:23 +0530

    Initial commit for project setup

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+/* eslint-disable */ 
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+    ],
+    "rules": {
+      "no-unused-vars": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off"
+    }
+};
